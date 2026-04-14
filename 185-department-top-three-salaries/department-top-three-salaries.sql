4793e42686d4aa216f9dae99b77a3276da96cfa3
Nishank garg <gargnishank88@gmail.com>
2026-04-14 20:33:58 +0530

    Delete 185-department-top-three-salaries directory

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++
@@ -1,9 +0,0 @@
-select d1.name as Department ,e1.name as Employee , e1.salary as Salary
-from Employee e1
-join Department d1 
-on e1.departmentId = d1.id
-where( 
-    select count(distinct e2.salary) from Employee e2
-where 
-e2.salary > e1.salary 
-and e1.departmentId = e2.departmentId) <3;
