9b8a05f1c63155f0f86e2664e778236b8afed99f
Nishank garg <gargnishank88@gmail.com>
2026-05-07 16:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (60.27%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,67 @@
+class Solution {
+
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+
+        int peak = peakelement(mountainArr);
+
+        int ans = binarysearch(mountainArr, target, 0, peak);
+
+        if (ans == -1) {
+            ans = binarysearch(mountainArr, target, peak + 1, mountainArr.length() - 1);
+        }
+
+        return ans;
+    }
+
+    int peakelement(MountainArray arr) {
+
+        int start = 0;
+        int end = arr.length() - 1;
+
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr.get(mid) > arr.get(mid + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+    int binarysearch(MountainArray arr, int target, int start, int end) {
+
+        boolean isAsc = arr.get(start) <= arr.get(end);
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr.get(mid) == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+
+                if (target > arr.get(mid)) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            } else {
+
+                if (target > arr.get(mid)) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
